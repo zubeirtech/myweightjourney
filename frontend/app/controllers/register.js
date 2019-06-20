e55379cba56acfc3f37b1b67ebe68773278f7919
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-06-20 18:13:54 +0200

    finalize authentication

--- frontend/app/controllers/register.js
+++ frontend/app/controllers/register.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
 import { set } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Ember from 'ember';
+
 
 export default Controller.extend({
     toastr: service('toast'),
@@ -8,30 +10,46 @@ export default Controller.extend({
     init() {
         this._super(...arguments);
         this.message = [];
-    }, 
+    },
+
+    validate(email) {
+        // eslint-disable-next-line no-useless-escape
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    },
 
     actions: {
         async register() {
             if(!this.username || !this.email || !this.password || !this.secondPassword) {
-                this.message.pushObject('Please fill in all fields')
-                if (this.password === this.secondPassword) {
-                    this.message.pushObject('The Passwords dont match')
-                }
+                this.toastr.error("Please fill in all fields", "Error");
             } else {
-                if (this.password === this.secondPassword) {
-                    this.message.pushObject('The Passwords dont match')
+                if (this.password !== this.secondPassword) {
+                    this.toastr.error("Password don't match", "Error");
                 } else {
                     try {
-                        set(this.model, 'username', this.username);
-                        set(this.model, 'email', this.email);
-                        set(this.model, 'password', this.password);
-                        const user = await this.model.save();
-                        if (user) {
-                            this.toastr.success('Successfully added user', 'Congratulations!');
+                        const validation = this.validate(this.email);
+                        if (validation) {
+                            set(this.model, 'username', this.username);
+                            set(this.model, 'email', this.email);
+                            set(this.model, 'password', this.password);
+                            await this.model.save()
+                              .then(doc => {
+                                  if (doc) {
+                                      this.toastr.success("Successfully added user", "Congratulations");
+                                  }
+                              })
+                              .catch(e => {
+                                  Ember.Logger.warn(e);
+                                  this.toastr.error("Email already exists, please use different email", "Error");                            
+                              })
+
+                            
+                        } else {
+                            this.toastr.error("Please use valid email", "Error");
                         }
+
                     } catch (e) {
-                        console.log(e);
-                        this.toastr.error(e, 'Error!');
+                        throw new e;
                     }
                 }
             }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
+const bcrypt = require('bcrypt');
 
 
 const router = express.Router();
@@ -12,18 +13,32 @@ const User = require('../models/User');
 // token route for auth
 router.post('/token', asyncHandler(async (req, res, next) => {
     if (req.body.grant_type === 'password') {
-        const { username, password } = req.body;
-
-        await User.find({ email: username, password }, (err, docs) => {
-            console.log(docs);
-            if (docs.length !== 0) {
-                res.status(200).send('{ "access_token": "secret token"}');
-                next();
-            } else {
-                res.status(400).send('{"error": "invalid_grant"}');
-                next();
-            }
-        });
+        try {
+            const { username, password } = req.body;
+            await User.find({ email: username }, (err, docs) => {
+                console.log(docs);
+                if (docs.length !== 0) {
+                    // eslint-disable-next-line max-len
+                    bcrypt.compare(password, docs[0].password, (error, val) => {
+                        if (error) {
+                            next(error);
+                        }
+                        if (val) {
+                            res.status(200).send('{ "access_token": "secret token"}');
+                            next();
+                        } else {
+                            res.status(400).send('{"error": "invalid_grant"}');
+                            next();
+                        }
+                    });
+                } else {
+                    res.status(400).send('{"error": "invalid_grant"}');
+                    next();
+                }
+            });
+        } catch (error) {
+            next(error);
+        }
     } else {
         res.status(400).send('{ "error": "unsupported_grant_type" }');
     }
@@ -36,21 +51,48 @@ router.post('/users', asyncHandler((req, res, next) => {
     });
 
     new JSONAPIDeserializer().deserialize(req.body, async (err, user) => {
-        if (user) {
-            const { username, email, password } = user;
-            const newUser = [{ username, email, password }];
 
-            const insertUser = await User.insertMany(newUser);
+        try {
+            if (user) {
+                const { username, email } = user;
+                let { password } = user;
 
-            if (insertUser) {
-                const usersJson = UserSerializer.serialize(insertUser);
-                res.status(200).json(usersJson);
+                const findUser = await User.find({ email });
+
+                if (findUser.length > 0) {
+                    res.status(404).send('{"error": "E-mail exists already"}');
+                    next();
+                } else {
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(password, salt, async (error, hash) => {
+                            if (error) {
+                                next(error);
+                            }
+                            password = hash;
+
+                            const newUser = new User({
+                                username,
+                                email,
+                                password,
+                            });
+
+                            const saveUser = await newUser.save();
+
+                            if (saveUser) {
+                                const usersJson = UserSerializer.serialize(saveUser);
+                                res.status(200).json(usersJson);
+                                next();
+                            }
+                        });
+                    });
+                }
+            } else {
+                console.log(err);
+                res.status(500).send(err);
                 next();
             }
-        } else {
-            console.log(err);
-            res.status(500).send(err);
-            next();
+        } catch (error) {
+            next(error);
         }
     });
 }));
